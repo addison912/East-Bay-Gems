961104ac44fa57e910776fdfb86bc1ee4a467b1a
Addison Moore <addisondrewmoore@gmail.com>
2018-12-03 22:40:51 -0800

    added CRUD functionality

--- server.js
+++ server.js
@@ -16,10 +16,6 @@ app.use(express.json());
 // body parser config to accept our datatypes
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
-
-
 ////////////////////
 //  ROUTES
 ///////////////////
@@ -44,6 +40,8 @@ app.get("/api/cities", (req, res) => {
     }
   });
 });
+
+//get all places
 app.get("/api/places", (req, res) => {
   db.Places.find().exec(function(err, places) {
     if (err) {
@@ -54,25 +52,38 @@ app.get("/api/places", (req, res) => {
     }
   });
 });
+
+// get featured places
 app.get("/api/places/featured", (req, res) => {
   db.Places.find().exec(function(err, places) {
+    let featured = [];
     if (err) {
-      console.log("index error: " + err);
       res.sendStatus(500);
-    } else {
-      places.forEach(featPlace => {
-        if(featPlace.isFeatured == true){
-          console.log(featPlace);
-          
-        };
-        res.json(places)
-      });
-      
+      return console.log("index error: " + err);
     }
+    places.forEach(featPlace => {
+      if (featPlace.isFeatured == true) {
+        console.log(featPlace);
+        featured.push(featPlace);
+      }
+    });
+    res.json(featured);
   });
 });
 
-app.post('/api/places', (req, res)=>{
+// get one place
+app.get("/api/places/:id", function(req, res) {
+  // find one place by its id
+  db.Places.findOne({ _id: req.params.id }, (err, foundPlace) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.json(foundPlace);
+  });
+});
+
+//create a place
+app.post("/api/places", (req, res) => {
   var newPlace = new db.Places({
     name: req.body.name,
     type: req.body.type,
@@ -82,16 +93,46 @@ app.post('/api/places', (req, res)=>{
     photo: req.body.photo
   });
   console.log(req.body.city);
-  newPlace.save(function(err, place){
+  newPlace.save(function(err, place) {
     if (err) {
       console.log("create error: " + err);
     }
     console.log("created ", place.name);
   });
-})
+});
 
+// delete a place
+app.delete("/api/places/:id", function(req, res) {
+  // get place id from url params (`req.params`)
+  console.log("places", req.params);
+  var placeId = req.params.id;
+  // find the index of the place we want to remove
+  db.Places.findByIdAndDelete(placeId, (err, deletedPlace) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.json(deletedPlace);
+  });
+  console.log("deleting place with id", placeId);
+});
 
-
+// update place
+app.put("/api/places/:id", function(req, res) {
+  // get place id from url params (`req.params`)
+  var placeId = req.params.id;
+  // find the index of the place we want to update
+  db.Places.findByIdAndUpdate(
+    placeId,
+    req.body,
+    { new: true },
+    (err, updatedPlace) => {
+      if (err) {
+        return console.log(err);
+      }
+      res.json(updatedPlace);
+    }
+  );
+});
 
 //run server
 let port = process.env.PORT || 3000;
