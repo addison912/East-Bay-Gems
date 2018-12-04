2d925fad43ad9aee360edf1475f6cf662f1dfaa0
Addison Moore <addisondrewmoore@gmail.com>
2018-12-03 19:32:04 -0800

    pulled login functions out of app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -62,34 +62,3 @@ $(document).ready(function() {
 $(document).ready(function() {
   $(".modal").modal();
 });
-
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log("Name: " + profile.getName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
-  $("#sign-in-button").toggleClass("hide");
-  $("#nav-profile-pic").toggleClass("hide");
-  document.getElementById(
-    "nav-profile-pic"
-  ).style.backgroundImage = `url("${profile.getImageUrl()}")`;
-}
-
-function onSuccess(googleUser) {
-  console.log("Logged in as: " + googleUser.getBasicProfile().getName());
-}
-function onFailure(error) {
-  console.log(error);
-}
-function renderButton() {
-  gapi.signin2.render("my-signin2", {
-    scope: "profile email",
-    width: 240,
-    height: 50,
-    longtitle: true,
-    theme: "dark",
-    onsuccess: onSuccess,
-    onfailure: onFailure
-  });
-}
