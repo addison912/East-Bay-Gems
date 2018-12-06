3859cf1c678c2c67f7d798657f540058ab1bb52e
Langdon <lfroker@gmail.com>
2018-12-06 09:19:40 -0800

    commiting changes to merge

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -27,6 +27,7 @@ $(document).ready(function() {
   $(".fixed-action-btn").floatingActionButton();
   $("select").formSelect();
   $(".sidenav").sidenav();
+
   $("#places").on("click", function() {
     $("#places_menu").toggleClass("hide");
     checkHidden();
@@ -34,17 +35,10 @@ $(document).ready(function() {
   $("#people").on("click", function() {
     checkHidden();
   });
-  $("#gemSelector").change(function() {
-    if ($(this).val() == "place") {
-      $("#submitPlace").removeClass("hide");
-      $("#submitPerson").addClass("hide");
-    }
-    if ($(this).val() == "person") {
-      $("#submitPerson").removeClass("hide");
-      $("#submitPlace").addClass("hide");
-    }
-  });
 
+
+  
+/* ///////////AJAX calls to populate page/////////// */
   $.ajax({
     method: "GET",
     url: "/api/places",
@@ -68,12 +62,21 @@ $(document).ready(function() {
   function placeError() {
     console.log("error");
   }
-  function featError() {
-    console.log("error");
-  }
   function peopleError() {
     console.log("error");
   }
+
+  // Shows correct form
+  $("#gemSelector").change(function() {
+    if ($(this).val() == "place") {
+      $("#submitPlace").removeClass("hide");
+      $("#submitPerson").addClass("hide");
+    }
+    if ($(this).val() == "person") {
+      $("#submitPerson").removeClass("hide");
+      $("#submitPlace").addClass("hide");
+    }
+  });
 
   // post new place
   $("#newPlaceForm").on("submit", function(e) {
@@ -88,6 +91,7 @@ $(document).ready(function() {
       console.log(json);
     }
   });
+  //post new person
   $("#newPersonForm").on("submit", function(e) {
     $.ajax({
       method: "POST",
@@ -103,9 +107,8 @@ $(document).ready(function() {
 
   var user = JSON.parse(sessionStorage.getItem("currentUser"));
   console.log(user);
-  ///////////////////////////////////////////////
-  ///////////////////////////////////////////////
-  ///////////////////////////////////////////////
+  
+  // Add's likes to users likes array when user like a gem
   $("#gems").on("click", ".halfway-fab", function() {
     let gem = this.name;
     let likes;
@@ -136,9 +139,8 @@ $(document).ready(function() {
     
     }
   });
-  ///////////////////////////////////////////////
-  ///////////////////////////////////////////////
 
+  //Search function
   $("#search").on("keyup", function() {
     var value = $(this)
       .val()
@@ -154,7 +156,7 @@ $(document).ready(function() {
     });
   });
 });
-
+//More magic
 let shuffle = array => {
   var m = array.length,
     t,
@@ -172,41 +174,33 @@ let shuffle = array => {
   return array;
 };
 
+//
 let populate = () => {
   gems = allPeople.concat(allPlaces);
   results = shuffle(gems);
   results.forEach(gem => {
-    cardHtml = `<div attr="${gem.city}" class="${
-      gem.gem
-    } card small horizontal hoverable" id=${gem._id}>
+    cardHtml = `<div attr="${gem.city}" class="${gem.gem} card small horizontal hoverable" id=${gem._id}>
                   <div class="card-image waves-effect waves-block waves-light">
                   </div>
                   <div class="card-stacked">
-                    <div class="card-content"><a name="${
-                      gem._id
-                    }" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-                      <span class="card-title activator grey-text text-darken-4"><i class="far fa-gem fa-1x top"></i> ${
-                        gem.name
-                      } - ${gem.city}</span>
+                    <div class="card-content"><a name="${gem._id}" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                      <span class="card-title activator grey-text text-darken-4"><i class="far fa-gem fa-1x top"></i> ${gem.name} - ${gem.city}</span>
                       <p>${gem.description}</p>
                     </div>
                     <div class="card-action">
                       <a href="${gem.url}">More info</a>
-                
                     </div>
                   </div>
                   <div class="card-reveal col l4">
-                      <span class="card-title grey-text text-darken-4">Lat=, Lon=<i class="material-icons right">close</i></span>
-                      <p>Info</p>
-                    </div>
+                    <span class="card-title grey-text text-darken-4">Lat=, Lon=<i class="material-icons right">close</i></span>
+                    <p>Info</p>
+                  </div>
                 </div>`;
     if (document.getElementById("gems")) {
       $("#gems").append(cardHtml);
       document
         .getElementById(`${gem._id}`)
-        .querySelector(".card-image").style.backgroundImage = `url("${
-        gem.photo
-      }")`;
+        .querySelector(".card-image").style.backgroundImage = `url("${gem.photo}")`;
     }
   });
 };
