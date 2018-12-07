f7ed064e2e8d61f35b0b9e4e6d2299082f4cd8ac
Addison Moore <addisondrewmoore@gmail.com>
2018-12-07 15:05:48 -0800

    added real objects to seed file

--- seed.js
+++ seed.js
@@ -7,7 +7,8 @@ var places_list = [
     description:
       "The Albany Bulb (also simply known as The Bulb) is a former landfill largely owned by the City of Albany, in California. The Bulb is the west end of a landfill peninsula jutting west from the east shore of San Francisco Bay. The term 'Bulb' is often used to refer to the entire peninsula, which includes the Albany Plateau, north of Buchanan Street at its base; the high narrow 'Neck,' and the round 'Bulb.' The Bulb is part of the City of Albany, and can be reached via Buchanan Street or the Bay Trail along the east side of San Francisco Bay.",
     city: "Albany",
-    photo: "https://i.imgur.com/vkPLCG7.jpg",
+    photo:
+      "http://1.bp.blogspot.com/-PM3kTTZnLQE/VdBp8o8PHvI/AAAAAAAAyv4/9WPyrkpmXA0/s1600/P1700521.jpg",
     url: "https://en.wikipedia.org/wiki/Albany_Bulb",
     coordinates: { lat: 37.5324, lng: -122.1929 },
     isFeatured: true,
@@ -50,6 +51,26 @@ var places_list = [
     coordinates: { lat: 37.8659, lng: -122.2365 },
     isFeatured: true,
     gem: "place"
+  },
+  {
+    type: ["restaurant"],
+    name: "Stay Gold",
+    description:
+      "Stay Gold Deli is an Italian-American Deli counter, Central Texas BBQ spot, grocer, restaurant, coffee shop, market, arcade, and beer garden. We offer a variety of specialty items, ranging from smoked meats to house-made pickles to vegan items. There is a fully functional smoker grandfathered into the property, rumored to have been built by the same folks who built Everett & Jones’ smokers back in the day! Don’t forget to check out our events page for wine tasting, tap takeovers, pig roasts, parties and other fun stuff!",
+    city: "Oakland",
+    url: "http://www.staygolddeli.com",
+    photo:
+      "https://oaklandnorth.net/wp-content/uploads/2016/09/IMG_0397-600x400.jpg"
+  },
+  {
+    name: "Brazil Cafe",
+    type: ["Restaurant"],
+    description:
+      "If you mention Brazilian food when you're in the Bay Area, someone is sure to tell you all about Brazil Cafe. You can't miss our colorful Brazilian flags and our huge  menu. Our signs will draw you in, but it's our delicious, authentic Brazilian food that will keep you coming back again and again. Our customers become fast friends here at Brazil Cafe. Watch the world go by at one of our outdoor tables or have your food wrapped to go—either way, it'll knock you out.",
+    city: "Berkeley",
+    url: "https://www.pedrosbrazilcafe.com/",
+    photo:
+      "http://1.bp.blogspot.com/-mnDSsRYoOS8/Uho2nxw7Q6I/AAAAAAAAAYk/4V3JPbYO4K0/s640/P1080948.JPG"
   }
 ];
 
@@ -65,47 +86,25 @@ var people_list = [
     gem: "people"
   },
   {
-    name: "Green Man",
+    name: "Wavy Gravy",
     description:
-      "Pink Man (real name Michael Maxfield) is a local celebrity from the San Francisco Bay Area. He can be seen riding his unicycle around the cities of Berkeley, Oakland and San Francisco. He gets his name from the shocking pink unitard and cape he wears while he performs impromptu unicycle tricks in public places—spinning, engaging in sudden stops, riding down the street at high speeds, and carrying people on his back",
+      'Hugh Nanton Romney (born May 15, 1936), better known as Wavy Gravy, is an American entertainer and activist for peace, best known for his hippie appearance, personality and countercultural beliefs. His moniker was given to him by B.B. King at the Texas International Pop Festival in 1969. "It\'s worked pretty well through my life," he says, "except with telephone operators – I have to say \'Gravy, first initial W.\'"',
     city: "Berkeley",
-    photo: "https://media.giphy.com/media/eUoGmTHkmGv72/giphy.gif",
-    url: "https://en.wikipedia.org/wiki/Pink_Man",
+    photo: "http://www.seva.org/images/content/pagebuilder/wavy80-wavy2.jpg",
+    url: "https://en.wikipedia.org/wiki/Wavy_Gravy",
     isAlive: true,
-    gem: "people"
+    gem: "person"
   },
   {
-    name: "Orange Man",
+    name: "Huey Newton",
     description:
-      "Pink Man (real name Michael Maxfield) is a local celebrity from the San Francisco Bay Area. He can be seen riding his unicycle around the cities of Berkeley, Oakland and San Francisco. He gets his name from the shocking pink unitard and cape he wears while he performs impromptu unicycle tricks in public places—spinning, engaging in sudden stops, riding down the street at high speeds, and carrying people on his back",
-    city: "Berkeley",
+      "Huey Percy Newton was a revolutionary African-American political activist who, along with Bobby Seale, co-founded the Black Panther Party in 1965. He continued to pursue graduate studies, eventually earning a Ph.D. in social philosophy. In 1989 he was murdered in Oakland, California by Tyrone Robinson, a member of the Black Guerrilla Family, in a dispute over drug dealing.",
+    city: "Oakland",
     photo:
-      "https://cdn-images-1.medium.com/max/1600/1*mI7YNvyBsT6ZtTks3TaaKg.jpeg",
-    url: "https://en.wikipedia.org/wiki/Pink_Man",
-    isAlive: true,
-    gem: "people"
-  },
-  {
-    name: "Red Man",
-    description:
-      "Pink Man (real name Michael Maxfield) is a local celebrity from the San Francisco Bay Area. He can be seen riding his unicycle around the cities of Berkeley, Oakland and San Francisco. He gets his name from the shocking pink unitard and cape he wears while he performs impromptu unicycle tricks in public places—spinning, engaging in sudden stops, riding down the street at high speeds, and carrying people on his back",
-    city: "Berkeley",
-    photo:
-      "https://pbs.twimg.com/profile_images/1035390955011026946/-VTnbCes_400x400.jpg",
-    url: "https://en.wikipedia.org/wiki/Pink_Man",
-    isAlive: true,
-    gem: "people"
-  },
-  {
-    name: "Blue Man",
-    description:
-      "Pink Man (real name Michael Maxfield) is a local celebrity from the San Francisco Bay Area. He can be seen riding his unicycle around the cities of Berkeley, Oakland and San Francisco. He gets his name from the shocking pink unitard and cape he wears while he performs impromptu unicycle tricks in public places—spinning, engaging in sudden stops, riding down the street at high speeds, and carrying people on his back",
-    city: "Berkeley",
-    photo:
-      "https://cdn2.atlantamagazine.com/wp-content/uploads/sites/4/2015/04/0415_bluemangroup_oneuseonly.jpg",
-    url: "https://en.wikipedia.org/wiki/Pink_Man",
-    isAlive: true,
-    gem: "people"
+      "http://cdn.vashtie.com/blog/wp-content/uploads/2016/02/1989huey.jpg",
+    url: "https://en.wikipedia.org/wiki/Huey_P._Newton",
+    isAlive: false,
+    gem: "person"
   }
 ];
 
